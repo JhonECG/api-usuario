e2795b7f62affef3f7fe1b4e6c47570737c0a594
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-07-13 20:03:31 -0500

    Ahora usamos tenant_id y no empresa

--- login_usuario.py
+++ login_usuario.py
@@ -53,7 +53,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {'Access-Control-Allow-Origin': '*'},
-            'body': json.dumps({'token': token})
+            'body': json.dumps({'token': token, 'user_id': user_id})
         }
 
     except Exception as e:
